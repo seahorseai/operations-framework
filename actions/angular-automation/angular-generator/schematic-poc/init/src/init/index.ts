519e9706794047a73e4d8e04f7db4ce9aef0c378
Jaime Gómez <jaimegomez@Jaimes-MacBook-Pro.local>
2025-01-04 20:27:49 +0100

    angular and nest automation deleted

--- actions/angular-automation/angular-generator/schematic-poc/init/src/init/index.ts
+++
@@ -1,9 +0,0 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
-
-export function init(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    tree.create('hello.js', `console.log('Hi ${_options.name}Frontend Rules');`);
-    return tree;
-  };
-}
